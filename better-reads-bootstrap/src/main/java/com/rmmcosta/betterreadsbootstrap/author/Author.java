d0ec35f3516d9fb36b44d4058355412a17d6ae01
rmmcosta <ricardocosta101085@gmail.com>
2024-02-29 08:11:07 +0000

    refactor: update the author table columns

--- better-reads-bootstrap/src/main/java/com/rmmcosta/betterreadsbootstrap/author/Author.java
+++ better-reads-bootstrap/src/main/java/com/rmmcosta/betterreadsbootstrap/author/Author.java
@@ -3,15 +3,23 @@ package com.rmmcosta.betterreadsbootstrap.author;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.*;
+import org.springframework.data.cassandra.core.mapping.CassandraType.Name;
 
-@Table
+@Table(value = "author_by_id")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Author {
-    @PrimaryKey
-    private String key;
+    @Id
+    @PrimaryKeyColumn(name = "author_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private String id;
+    @Column("author_name")
+    @CassandraType(type = Name.TEXT)
     private String name;
+    @Column("personal_name")
+    @CassandraType(type = Name.TEXT)
+    private String personalName;
 }
